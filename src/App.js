acde539d7960e541f6ff5b953da15cdb83967bf9
Mir Sujat <mir.sujat@gmail.com>
2020-08-09 20:28:10 +0600

    create accordion

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Accordion from "./components/Accordion/Accordion";
 
 import "./App.css";
 
@@ -6,6 +7,59 @@ function App() {
   return (
     <div data-testid="app">
       <h1>Hello World</h1>
+      <h1>Accordion Demo</h1>
+      <h2>Default</h2>
+      <Accordion>
+        <div label="Alligator Mississippiensis">
+          <p>
+            <strong>Common Name:</strong> American Alligator
+          </p>
+          <p>
+            <strong>Distribution:</strong> Texas to North Carolina, United
+            States
+          </p>
+          <p>
+            <strong>Endangered Status:</strong> Currently Not Endangered
+          </p>
+        </div>
+        <div label="Alligator Sinensis">
+          <p>
+            <strong>Common Name:</strong> Chinese Alligator
+          </p>
+          <p>
+            <strong>Distribution:</strong> Eastern China
+          </p>
+          <p>
+            <strong>Endangered Status:</strong> Critically Endangered
+          </p>
+        </div>
+      </Accordion>
+      <h2>Allow Multiple Open + Open Section By Default</h2>
+      <Accordion allowMultipleOpen>
+        <div label="Alligator Mississippiensis" isOpen>
+          <p>
+            <strong>Common Name:</strong> American Alligator
+          </p>
+          <p>
+            <strong>Distribution:</strong> Texas to North Carolina, United
+            States
+          </p>
+          <p>
+            <strong>Endangered Status:</strong> Currently Not Endangered
+          </p>
+        </div>
+        <div label="Alligator Sinensis">
+          <p>
+            <strong>Common Name:</strong> Chinese Alligator
+          </p>
+          <p>
+            <strong>Distribution:</strong> Eastern China
+          </p>
+          <p>
+            <strong>Endangered Status:</strong> Critically Endangered
+          </p>
+        </div>
+      </Accordion>
     </div>
   );
 }
