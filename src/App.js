6511ddce492ebd84850c4182541fc796e9c90593
Mir Sujat <mir.sujat@gmail.com>
2020-08-11 15:38:47 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from "react";
-import Accordion from "./components/Accordion/Accordion";
-import Tabs from "./components/Tabs/Tabs";
 
 import "./App.css";
 
@@ -8,80 +6,6 @@ function App() {
   return (
     <div data-testid="app">
       <h1>Hello World</h1>
-      <h1>Accordion Demo</h1>
-      <h2>Default</h2>
-      <Accordion>
-        <div label="Alligator Mississippiensis">
-          <p>
-            <strong>Common Name:</strong> American Alligator
-          </p>
-          <p>
-            <strong>Distribution:</strong> Texas to North Carolina, United
-            States
-          </p>
-          <p>
-            <strong>Endangered Status:</strong> Currently Not Endangered
-          </p>
-        </div>
-        <div label="Alligator Sinensis">
-          <p>
-            <strong>Common Name:</strong> Chinese Alligator
-          </p>
-          <p>
-            <strong>Distribution:</strong> Eastern China
-          </p>
-          <p>
-            <strong>Endangered Status:</strong> Critically Endangered
-          </p>
-        </div>
-      </Accordion>
-      <h2>Allow Multiple Open + Open Section By Default</h2>
-      <Accordion allowMultipleOpen>
-        <div label="Alligator Mississippiensis" isOpen>
-          <p>
-            <strong>Common Name:</strong> American Alligator
-          </p>
-          <p>
-            <strong>Distribution:</strong> Texas to North Carolina, United
-            States
-          </p>
-          <p>
-            <strong>Endangered Status:</strong> Currently Not Endangered
-          </p>
-        </div>
-        <div label="Alligator Sinensis">
-          <p>
-            <strong>Common Name:</strong> Chinese Alligator
-          </p>
-          <p>
-            <strong>Distribution:</strong> Eastern China
-          </p>
-          <p>
-            <strong>Endangered Status:</strong> Critically Endangered
-          </p>
-        </div>
-      </Accordion>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <Tabs>
-        <div label="Gator">
-          See ya later, <em>Alligator</em>!
-        </div>
-        <div label="Croc">
-          After &apos;while, <em>Crocodile</em>!
-        </div>
-        <div label="Sarcosuchus">
-          Nothing to see here, this tab is <em>extinct</em>!
-        </div>
-      </Tabs>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
     </div>
   );
 }
