b23322109795b5a95b90696435dea5af377fcf36
Mir Sujat <mir.sujat@gmail.com>
2020-08-12 16:45:26 +0600

    Update TodoAccordSection.js

--- src/components/Todo/TodoAccordSection.js
+++ src/components/Todo/TodoAccordSection.js
@@ -1,9 +1,19 @@
 import React from "react";
 
 const TodoAccordSection = (props) => {
+  const onClick = () => {
+    props.onClick(props.title);
+  };
   return (
     <div className="accord_section">
-      <h1>accordion section</h1>
+      <div className="accord_section" isopen={props.isopen}>
+        <div onClick={onClick} className="title" style={{ cursor: "pointer" }}>
+          {props.title}
+        </div>
+        {props.isopen ? (
+          <div className="section_content">{props.children}</div>
+        ) : null}
+      </div>
     </div>
   );
 };
