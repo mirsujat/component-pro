18c445e7f4913b6f0f2d6d846131b4dda7acc7d2
Mir Sujat <mir.sujat@gmail.com>
2020-08-12 16:45:01 +0600

    Update TodoAccord.js

--- src/components/Todo/TodoAccord.js
+++ src/components/Todo/TodoAccord.js
@@ -1,12 +1,47 @@
 import React, { Component } from "react";
+import TodoAccordSection from "./TodoAccordSection";
 
 class TodoAccord extends Component {
+  static defaultProps = { allowMultipleOpen: false };
   constructor(props) {
     super(props);
-    this.state = {};
+    const openSections = {};
+    this.props.children.forEach((child) => {
+      if (child.props.isopen) {
+        openSections[child.props.title] = true;
+      }
+    });
+    this.state = { openSections };
   }
+  onClick = (title) => {
+    const { openSections } = this.state;
+    const isopen = openSections[title];
+    if (this.props.allowMultipleOpen) {
+      this.setState({ openSections: { ...openSections, [title]: !isopen } });
+    } else {
+      this.setState({ openSections: { [title]: !isopen } });
+    }
+  };
+
   render() {
-    return <div className="accord">{this.props.children}</div>;
+    const { children } = this.props;
+    const { openSections } = this.state;
+    return (
+      <div className="accord">
+        {children.map((child, i) => {
+          return (
+            <TodoAccordSection
+              title={child.props.title}
+              onClick={this.onClick}
+              isopen={openSections[child.props.title]}
+              key={i}
+            >
+              {child.props.children}
+            </TodoAccordSection>
+          );
+        })}
+      </div>
+    );
   }
 }
 
