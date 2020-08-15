cc2fb41bfc079afea104f24408d09411212d6911
Mir Sujat <mir.sujat@gmail.com>
2020-08-15 12:03:51 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,18 +8,6 @@ function App() {
     <div data-testid="app" className="app">
       <h1>Hello World</h1>
       <br></br>
-      <h3>Tabs</h3>
-      <Tab>
-        <div label="Tab One">
-          <p>Hello From Tab One</p>
-        </div>
-        <div label="Tab Two">
-          <p>Hi! Tab Two</p>
-        </div>
-        <div label="Tab three">
-          <p>it's me Tab Three</p>
-        </div>
-      </Tab>
     </div>
   );
 }
