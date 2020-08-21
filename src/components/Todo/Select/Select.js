ce20ddd8cbadf35273a2f3b5d9f438610aed13a2
Mir Sujat <mir.sujat@gmail.com>
2020-08-21 20:11:25 +0600

    cleanup

--- src/components/Todo/Select/Select.js
+++ src/components/Todo/Select/Select.js
@@ -15,55 +15,28 @@ class Select extends Component {
       isOpen: false,
     };
     this.toggleContainer = React.createRef();
-    this.onClickHandler = this.onClickHandler.bind(this);
-    this.onClickOutsideHandler = this.onClickOutsideHandler.bind(this);
   }
 
-  componentDidMount() {
-    window.addEventListener("click", this.onClickOutsideHandler);
-  }
+  componentDidMount = () => {
+    window.addEventListener("click", this.onClickOutsideToHidePopup);
+  };
 
-  // componentDidUpdate(prevProps) {
-  //   const { options, selectedValues } = this.props;
-  //   const {
-  //     options: prevOptions,
-  //     selectedValues: prevSelectedvalues,
-  //   } = prevProps;
-  //   if (JSON.stringify(prevOptions) !== JSON.stringify(options)) {
-  //     this.setState({
-  //       options,
-  //     });
-  //   }
-  //   if (JSON.stringify(prevSelectedvalues) !== JSON.stringify(selectedValues)) {
-  //     this.setState({
-  //       selectedValues: Object.assign([], selectedValues),
-  //       preSelectedValues: Object.assign([], selectedValues),
-  //     });
-  //   }
-  // }
+  componentWillUnmount = () => {
+    window.removeEventListener("click", this.onClickOutSideToHidePopup);
+  };
 
-  componentWillUnmount() {
-    window.removeEventListener("click", this.onClickOutsideHandler);
-  }
+  onClickinSideToShowPopup = () => {
+    this.setState({ isOpen: true });
+  };
 
-  onClickHandler() {
-    this.setState((currentState) => ({
-      isOpen: !currentState.isOpen,
-    }));
-  }
-
-  onClickOutsideHandler(event) {
+  onClickOutSideToHidePopup = (event) => {
     if (
       this.state.isOpen &&
       !this.toggleContainer.current.contains(event.target)
     ) {
       this.setState({ isOpen: false });
     }
-  }
-
-  //  onChangeHandler = (event) => {
-  //     this.props.handleCheckboxChange(event);
-  //   };
+  };
 
   createCheckbox = (option) => (
     <Checkbox
@@ -83,7 +56,7 @@ class Select extends Component {
               className="select_input"
               type="text"
               placeholder="Select an Option"
-              onClick={this.onClickHandler}
+              onClick={this.onClickinSideToShowPopup}
             ></input>
             <ul className={this.state.isOpen ? "option_box" : "hidden"}>
               {this.state.isOpen && this.state.options.map(this.createCheckbox)}
@@ -94,7 +67,7 @@ class Select extends Component {
     );
   };
 
-  renderSelect() {
+  renderSelect = () => {
     if (this.props.isMultiple) return this.createCheckboxes();
     return (
       <select
@@ -109,7 +82,7 @@ class Select extends Component {
         <option value="bankTransfer">Bank Transfer</option>
       </select>
     );
-  }
+  };
 
   render() {
     return <div className="select_container">{this.renderSelect()}</div>;
