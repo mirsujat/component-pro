fff04be239897f78de85a8ae99060973c1403506
Mir Sujat <mir.sujat@gmail.com>
2020-08-24 16:58:12 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,35 +3,32 @@ import React, { Component } from "react";
 import "./App.css";
 import Select from "./components/Todo/Select/Select";
 
-const OPTIONS = ["Coding", "Music", "Sports", "Arts", "Cooking", "Travel"];
+const INTERESTS = [
+  { id: 1, value: "Coding", isChecked: false },
+  { id: 2, value: "Reading", isChecked: false },
+  { id: 3, value: "Writing", isChecked: false },
+  { id: 4, value: "Sports", isChecked: false },
+  { id: 5, value: "Movies", isChecked: false },
+  { id: 6, value: "Travelling", isChecked: false },
+];
+
 class App extends Component {
   state = {
     PaymentMethod: "noPaymentMethod",
-    options: [...OPTIONS],
-    checkboxes: OPTIONS.reduce(
-      (options, option) => ({
-        ...options,
-        [option]: false,
-      }),
-      {}
-    ),
+    options: [...INTERESTS],
   };
 
-  onChangeHandler = (event) => {
-    const { name } = event.target;
-    //Multiselect
+  onChange = (event) => {
+    const options = this.state.options;
+    const { name, value } = event.target;
     if (event.target.type === "checkbox") {
-      this.setState((prevState) => ({
-        checkboxes: {
-          ...prevState.checkboxes,
-          [name]: !prevState.checkboxes[name],
-        },
-      }));
-    } else {
-      //Default select and other input
-      this.setState({
-        [name]: event.target.value,
+      options.forEach((option) => {
+        if (option.value === event.target.value)
+          option.isChecked = event.target.checked;
       });
+      this.setState({ options: options });
+    } else {
+      this.setState({ [name]: value });
     }
   };
 
@@ -51,10 +48,7 @@ class App extends Component {
           isMultiple
           chips
           options={this.state.options}
-          checkboxes={this.state.checkboxes}
-          onChange={this.onChangeHandler}
-          selectedValue={this.state.PaymentMethod}
-          name="PaymentMethod"
+          onChange={this.onChange}
         ></Select>
       </div>
     );
